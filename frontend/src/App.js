a3ce34b29c037da697e1759a553617e7c5bdc458
Jovan <mister.jovan@gmail.com>
2023-11-06 18:39:56 -0800

    login modal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
-import LoginFormPage from "./components/LoginFormPage";
+//import LoginFormPage from "./components/LoginFormPage";
 import SignupFormPage from "./components/SignupFormPAge";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
@@ -18,17 +18,14 @@ function App() {
 
   return (
     <>
-    <Navigation isLoaded={isLoaded} />
-    {isLoaded && (
-      <Switch>
-        <Route path="/login">
-          <LoginFormPage />
-        </Route>
-        <Route path="/signup">
-          <SignupFormPage />
-        </Route>
-      </Switch>
-    )}
+      <Navigation isLoaded={isLoaded} />
+      {isLoaded && (
+        <Switch>
+          <Route path="/signup">
+            <SignupFormPage />
+          </Route>
+        </Switch>
+      )}
     </>
   );
 }
