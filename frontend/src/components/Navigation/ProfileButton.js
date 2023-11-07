28ab40945b84e89f1546506e3dfff4a053be1195
Jovan <mister.jovan@gmail.com>
2023-11-06 19:03:29 -0800

    phase 5

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -2,32 +2,32 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useDispatch } from 'react-redux';
 import * as sessionActions from '../../store/session';
+import OpenModalButton from '../OpenModalButton';
+import LoginFormModal from '../LoginFormModal';
+import SignupFormModal from '../SignupFormModal';
 
 function ProfileButton({ user }) {
   const dispatch = useDispatch();
   const [showMenu, setShowMenu] = useState(false);
-  const buttonRef = useRef(null); // Ref for the button
-  const dropdownRef = useRef(null); // Ref for the dropdown
+  const ulRef = useRef();
 
   const openMenu = () => {
-    if (!showMenu) setShowMenu(true);
+    if (showMenu) return;
+    setShowMenu(true);
   };
 
   useEffect(() => {
+    if (!showMenu) return;
+
     const closeMenu = (e) => {
-      // Check if the click is inside the dropdown, if not then close the menu
-      if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
+      if (!ulRef.current.contains(e.target)) {
         setShowMenu(false);
       }
     };
 
-    // If the menu is shown, then add the event listener
-    if (showMenu) {
-      document.addEventListener('mousedown', closeMenu);
-    }
+    document.addEventListener('click', closeMenu);
 
-    // Cleanup the event listener when the component is unmounted or when the menu is closed
-    return () => document.removeEventListener('mousedown', closeMenu);
+    return () => document.removeEventListener("click", closeMenu);
   }, [showMenu]);
 
   const logout = (e) => {
@@ -35,22 +35,100 @@ function ProfileButton({ user }) {
     dispatch(sessionActions.logout());
   };
 
+  const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+
   return (
     <>
-      <button onClick={openMenu} ref={buttonRef}>
-        <i className="fas fa-user-circle" /> {user.username}
+      <button onClick={openMenu}>
+        <i className="fas fa-user-circle" />
       </button>
-      {showMenu && (
-        <ul className="profile-dropdown" ref={dropdownRef}>
-          <li>{user.username}</li>
-          <li>{user.email}</li>
-          <li>
-            <button onClick={logout}>Log Out</button>
-          </li>
-        </ul>
-      )}
+      <ul className={ulClassName} ref={ulRef}>
+        {user ? (
+          <>
+            <li>{user.username}</li>
+            <li>{user.firstName} {user.lastName}</li>
+            <li>{user.email}</li>
+            <li>
+              <button onClick={logout}>Log Out</button>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <OpenModalButton
+                buttonText="Log In"
+                modalComponent={<LoginFormModal />}
+              />
+            </li>
+            <li>
+              <OpenModalButton
+                buttonText="Sign Up"
+                modalComponent={<SignupFormModal />}
+              />
+            </li>
+          </>
+        )}
+      </ul>
     </>
   );
 }
 
 export default ProfileButton;
+
+
+// // frontend/src/components/Navigation/ProfileButton.js
+// import React, { useState, useEffect, useRef } from "react";
+// import { useDispatch } from 'react-redux';
+// import * as sessionActions from '../../store/session';
+
+// function ProfileButton({ user }) {
+//   const dispatch = useDispatch();
+//   const [showMenu, setShowMenu] = useState(false);
+//   const buttonRef = useRef(null); // Ref for the button
+//   const dropdownRef = useRef(null); // Ref for the dropdown
+
+//   const openMenu = () => {
+//     if (!showMenu) setShowMenu(true);
+//   };
+
+//   useEffect(() => {
+//     const closeMenu = (e) => {
+//       // Check if the click is inside the dropdown, if not then close the menu
+//       if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
+//         setShowMenu(false);
+//       }
+//     };
+
+//     // If the menu is shown, then add the event listener
+//     if (showMenu) {
+//       document.addEventListener('mousedown', closeMenu);
+//     }
+
+//     // Cleanup the event listener when the component is unmounted or when the menu is closed
+//     return () => document.removeEventListener('mousedown', closeMenu);
+//   }, [showMenu]);
+
+//   const logout = (e) => {
+//     e.preventDefault();
+//     dispatch(sessionActions.logout());
+//   };
+
+//   return (
+//     <>
+//       <button onClick={openMenu} ref={buttonRef}>
+//         <i className="fas fa-user-circle" /> {user.username}
+//       </button>
+//       {showMenu && (
+//         <ul className="profile-dropdown" ref={dropdownRef}>
+//           <li>{user.username}</li>
+//           <li>{user.email}</li>
+//           <li>
+//             <button onClick={logout}>Log Out</button>
+//           </li>
+//         </ul>
+//       )}
+//     </>
+//   );
+// }
+
+// export default ProfileButton;
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,50 +1,76 @@
 // frontend/src/components/Navigation/index.js
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
-import * as sessionActions from '../../store/session';
 import './Navigattion.css';
-import OpenModalButton from "../OpenModalButton";
-import LoginFormModal from "../LoginFormModal";
-import SignupFormModal from "../SignupFormModal";
 
+function Navigation({ isLoaded }){
+  const sessionUser = useSelector(state => state.session.user);
 
-function Navigation({ isLoaded }) {
-    const sessionUser = useSelector((state) => state.session.user);
-
-    let sessionLinks;
-    if (sessionUser) {
-      sessionLinks = (
+  return (
+    <ul>
+      <li>
+        <NavLink exact to="/">Home</NavLink>
+      </li>
+      {isLoaded && (
         <li>
           <ProfileButton user={sessionUser} />
         </li>
-      );
-    } else {
-      sessionLinks = (
-        <li>
-          <OpenModalButton
-            buttonText="Log In"
-            modalComponent={<LoginFormModal />}
-          />
-          <OpenModalButton
-          buttonText="Sign Up"
-          modalComponent={<SignupFormModal />}
-        />
-        </li>
-      );
-    }
+      )}
+    </ul>
+  );
+}
 
-    return (
-      <ul>
-        <li>
-          <NavLink exact to="/">
-            Home
-          </NavLink>
-        </li>
-        {isLoaded && sessionLinks}
-      </ul>
-    );
-  }
+export default Navigation;
 
-  export default Navigation;
+// // frontend/src/components/Navigation/index.js
+// import React from 'react';
+// import { NavLink } from 'react-router-dom';
+// import { useSelector, useDispatch } from 'react-redux';
+// import ProfileButton from './ProfileButton';
+// import * as sessionActions from '../../store/session';
+// import './Navigattion.css';
+// import OpenModalButton from "../OpenModalButton";
+// import LoginFormModal from "../LoginFormModal";
+// import SignupFormModal from "../SignupFormModal";
+
+
+// function Navigation({ isLoaded }) {
+//     const sessionUser = useSelector((state) => state.session.user);
+
+//     let sessionLinks;
+//     if (sessionUser) {
+//       sessionLinks = (
+//         <li>
+//           <ProfileButton user={sessionUser} />
+//         </li>
+//       );
+//     } else {
+//       sessionLinks = (
+//         <li>
+//           <OpenModalButton
+//             buttonText="Log In"
+//             modalComponent={<LoginFormModal />}
+//           />
+//           <OpenModalButton
+//           buttonText="Sign Up"
+//           modalComponent={<SignupFormModal />}
+//         />
+//         </li>
+//       );
+//     }
+
+//     return (
+//       <ul>
+//         <li>
+//           <NavLink exact to="/">
+//             Home
+//           </NavLink>
+//         </li>
+//         {isLoaded && sessionLinks}
+//       </ul>
+//     );
+//   }
+
+//   export default Navigation;
